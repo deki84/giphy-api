cb6dbd9912a123f6bd8bff550214e0ff6bbf9c00
Dejan Jankovic <deki1984@hotmail.de>
2023-02-17 15:04:19 +0100

    create methods search and searchGifs

--- src/app/gifs/gifs.component.ts
+++ src/app/gifs/gifs.component.ts
@@ -1,17 +1,25 @@
 import { DataService } from './../data.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gifs',
   templateUrl: './gifs.component.html',
   styleUrls: ['./gifs.component.css'],
 })
-export class GifsComponent implements OnInit {
-  gifs: any;
+export class GifsComponent implements OnInit, OnDestroy {
+  gifs: any[] = [];
+  subscription: Subscription = new Subscription();
   constructor(private dataService: DataService) {}
   ngOnInit(): void {
-    this.dataService.getTrendingGifs().subscribe((response: any) => {
-      this.gifs = response.data;
-    });
+    this.dataService.getTrendingGifs();
+    this.subscription = this.dataService
+      .getGifs()
+      .subscribe((response: any) => {
+        this.gifs = response;
+      });
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { DataService } from './../data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  constructor(private dataService: DataService) {}
 
+  ngOnInit(): void {}
+
+  search(searchTerm: string) {
+    if (searchTerm !== '') {
+      this.dataService.searchGifs(searchTerm);
+    }
+  }
 }
